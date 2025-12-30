031b0e7254e644f594bc40beb5e346ce1df9020d
MuGi-131 <munkhbayar.mugi@gmail.com>
2025-12-31 00:32:23 +0900

    get book by argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_num_chars, get_sorted_chars
 
 
@@ -10,7 +11,10 @@ def get_book_text(file_path):
 def main():
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     print("----------- Word Count ----------")
     get_num_words(text)
     print("--------- Character Count -------")
